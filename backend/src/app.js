a8151fa903892cd84c4bc23f9ac3158bf7791267
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2025-05-04 23:03:22 -0300

    cors adicionado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/studentRoutes.js';
 import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
